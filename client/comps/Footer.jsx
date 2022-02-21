b982c8ba0cd70053736bf634a004d4bed965185b
2M0N <tumon@Debian11.2M0N>
2022-02-21 21:52:11 +0600

    first commit

---
+++ client/comps/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Footer.module.css'
+
+const Footer = () => {
+  return (
+   <div className={styles.footerHold}>
+    <span className={styles.copyrights}>
+        Copyrights 2022
+    </span>
+   </div>
+  )
+}
+
+export default Footer
---
+++ client/pages/index.js
@@ -0,0 +1,23 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import Footer from '../comps/Footer'
+export default function Home() {
+  return (
+    <>
+     <div className={styles.headerHold}>
+     <div className={styles.h1}> Home Page </div>
+     <div className={styles.about}>
+      <Link href='/about'> About </Link> 
+     </div>
+     </div>
+     <p className={styles.p}>     
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magni dolor tempore, fugit delectus, laboriosam dolore sed assumenda nam animi error modi quidem, tenetur libero? Veritatis temporibus eius fugit quam.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magni dolor tempore, fugit delectus, laboriosam dolore sed assumenda nam animi error modi quidem, tenetur libero? Veritatis temporibus eius fugit quam.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magni dolor tempore, fugit delectus, laboriosam dolore sed assumenda nam animi error modi quidem, tenetur libero? Veritatis temporibus eius fugit quam.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magni dolor tempore, fugit delectus, laboriosam dolore sed assumenda nam animi error modi quidem, tenetur libero? Veritatis temporibus eius fugit quam.
+     </p>
+
+     <Footer />
+    </>
+  )
+}
