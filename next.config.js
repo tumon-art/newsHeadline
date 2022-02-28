269520834a374854274a287130f4df2fbc168c5f
2M0N <tumon@Debian11.2M0N>
2022-03-01 02:00:42 +0600

    init

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    NEWS_API:'4c772d8e7ba440caa89fa5862161df6c'
+    NEWS_API:'edad89887fb146608543a1054589c3dc'
   },
 }
 module.exports = nextConfig
